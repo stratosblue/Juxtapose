9e47ed6ba7d94e39a8a1984d89352f4a79f037ba
Stratos <StratosBlue@outlook.com>
2021-11-29 19:43:00 +0800

    add IIllusionExtensions to get IExternalProcess from IIllusion class

---
+++ src/Juxtapose/Extensions/IIllusionExtensions.cs
@@ -0,0 +1,25 @@
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace Juxtapose
+{
+    /// <summary>
+    ///
+    /// </summary>
+    public static class IIllusionExtensions
+    {
+        #region Public 方法
+
+        /// <summary>
+        /// 尝试获取关联的<see cref="IExternalProcess"/>
+        /// </summary>
+        /// <param name="illusion"></param>
+        /// <param name="externalProcess"></param>
+        /// <returns></returns>
+        public static bool TryGetExternalProcess(this IIllusion illusion, [NotNullWhen(true)] out IExternalProcess? externalProcess)
+        {
+            return illusion.Executor.TryGetExternalProcess(out externalProcess);
+        }
+
+        #endregion Public 方法
+    }
+}
