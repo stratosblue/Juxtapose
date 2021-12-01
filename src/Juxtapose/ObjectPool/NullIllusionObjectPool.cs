7ae8d920bf93ac4a9bf0dd11c2c669cc9b849359
Stratos <StratosBlue@outlook.com>
2021-12-01 19:26:32 +0800

    add NullIllusionObjectPool;

---
+++ src/Juxtapose/ObjectPool/NullIllusionObjectPool.cs
@@ -0,0 +1,52 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+
+namespace Juxtapose.ObjectPool
+{
+    /// <summary>
+    /// 空的幻象对象池
+    /// </summary>
+    public sealed class NullIllusionObjectPool<T>
+        : IIllusionObjectPool<T>
+        where T : IIllusion
+    {
+        #region 共享实例
+
+        /// <summary>
+        /// 共享实例
+        /// </summary>
+        public static NullIllusionObjectPool<T> Instance { get; } = new();
+
+        #endregion 共享实例
+
+        #region Public 属性
+
+        /// <inheritdoc/>
+        public int IdleCount { get; } = 0;
+
+        /// <inheritdoc/>
+        public int TotalCount { get; } = 0;
+
+        #endregion Public 属性
+
+        #region Public 方法
+
+        /// <inheritdoc/>
+        public void Dispose()
+        {
+        }
+
+        /// <inheritdoc/>
+        public Task<T?> GetAsync(CancellationToken cancellation = default)
+        {
+            return Task.FromResult<T?>(default);
+        }
+
+        /// <inheritdoc/>
+        public void Return(T? item)
+        {
+        }
+
+        #endregion Public 方法
+    }
+}
---
+++ test/Juxtapose.Test/NullIllusionObjectPoolTest.cs
@@ -0,0 +1,55 @@
+﻿using System.Threading.Tasks;
+
+using Juxtapose.ObjectPool;
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Juxtapose.Test
+{
+    [TestClass]
+    public class NullIllusionObjectPoolTest
+    {
+        #region Public 方法
+
+        [TestMethod]
+        public async Task ShouldNoException()
+        {
+            var pool = NullIllusionObjectPool<TestClass>.Instance;
+
+            for (int i = 0; i < 3; i++)
+            {
+                Assert.IsNull(await pool.GetAsync());
+                Assert.AreEqual(0, pool.TotalCount);
+                Assert.AreEqual(0, pool.IdleCount);
+                pool.Return(null);
+                pool.Dispose();
+            }
+        }
+
+        #endregion Public 方法
+
+        #region Private 类
+
+        private class TestClass : IIllusion
+        {
+            #region Public 属性
+
+            public JuxtaposeExecutor Executor { get; }
+
+            public bool IsAvailable { get; private set; } = true;
+
+            #endregion Public 属性
+
+            #region Public 方法
+
+            public void Dispose()
+            {
+                IsAvailable = false;
+            }
+
+            #endregion Public 方法
+        }
+
+        #endregion Private 类
+    }
+}
