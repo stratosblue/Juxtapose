98d645e2b4a79b6c9d98daa6da14d9d8eac441f1
Stratos <StratosBlue@outlook.com>
2022-01-11 22:19:16 +0800

    添加一个待使用的ProvideByServiceProviderAttribute

---
+++ src/Juxtapose/SourceGenerator/ProvideByServiceProviderAttribute.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+#pragma warning disable IDE0060 // 删除未使用的参数
+
+namespace Juxtapose.SourceGenerator
+{
+    /// <summary>
+    /// 指定哪些类型通过 DI (<see cref="IServiceProvider"/>) 提供，不生成具体构造方法
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Class, AllowMultiple = true, Inherited = false)]
+    public class ProvideByServiceProviderAttribute : Attribute
+    {
+        #region Public 构造函数
+
+        /// <summary>
+        /// <inheritdoc cref="ProvideByServiceProviderAttribute"/>
+        /// </summary>
+        /// <param name="types">由DI提供的类型列表</param>
+        public ProvideByServiceProviderAttribute(params Type[] types)
+        {
+        }
+
+        #endregion Public 构造函数
+    }
+}
