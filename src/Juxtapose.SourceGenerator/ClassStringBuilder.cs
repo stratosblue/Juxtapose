5900fb34698e22b2bf9b85cdb6758d6f49b2d38f
Stratos <StratosBlue@outlook.com>
2022-09-25 20:22:25 +0800

    Generator chore

--- src/Juxtapose.SourceGenerator/ClassStringBuilder.cs
+++ src/Juxtapose.SourceGenerator/ClassStringBuilder.cs
@@ -4,6 +4,16 @@ namespace Juxtapose.SourceGenerator;
 
 public class ClassStringBuilder : IndentStringBuilder
 {
+    #region Public 构造函数
+
+    public ClassStringBuilder(int capacity = 4096) : base(capacity)
+    {
+    }
+
+    #endregion Public 构造函数
+
+    #region Public 方法
+
     /// <summary>
     /// 在一个命名空间内执行委托<paramref name="action"/>
     /// <para/>
@@ -19,4 +29,6 @@ public class ClassStringBuilder : IndentStringBuilder
         AppendIndentLine($"namespace {name}");
         Scope(action);
     }
+
+    #endregion Public 方法
 }
